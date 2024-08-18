9c64c971ee7dcb80f31709ec075e10ab05e09a48
root <root@EsToDa.localdomain>
2024-08-18 16:47:50 +0300

    done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,8 @@
 # relationship_app/urls.py
 
 from django.urls import path
-from .views import list_books, LibraryDetailView, SignUpView
+import views
+from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -9,5 +10,5 @@ urlpatterns = [
     path('library/', LibraryDetailView.as_view(), name = "library-detail"),
     path('login/', LoginView.as_view(template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/login.html'), name = 'login'),
     path('logout/', LogoutView.as_view(template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/logout.html'), name = 'logout'),
-    path('register/', SignUpView.as_view(), name = "register")
+    path('register/', views.register.as_view(), name = "register")
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -30,7 +30,7 @@ class LibraryDetailView(DetailView):
         context['books'] = books
         return context
     
-class SignUpView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy
     template_name = 'relationship_app/register.html'
