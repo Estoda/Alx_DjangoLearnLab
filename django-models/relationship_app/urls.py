935e0503aac06c0ca9be91dc59dad814958344c2
root <root@EsToDa.localdomain>
2024-08-18 16:33:00 +0300

    done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,13 @@
+# relationship_app/urls.py
+
 from django.urls import path
-from . import views
+from .views import book_list_view, LibraryDetailView, SignUpView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('books/', book_list_view, name='book-list'),
+    path('library/', LibraryDetailView.as_view(), name = "library-detail"),
+    path('login/', LoginView.as_view(template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/login.html'), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/logout.html'), name = 'logout'),
+    path('register/', SignUpView.as_view(), name = "register")
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,4 +1,35 @@
+# relationship_app/views.py
+
 from django.shortcuts import render
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Welcom to relationship_app page")
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+def book_list_view(request):
+    books = Book.objects.all()  # Query to get all books
+    context = {
+        'books': books,
+    }
+    return render(request, '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/book_list.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_object(self):
+        return Library.objects.get(name = "Cairo")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        books = library.books.all()
+        context['books'] = books
+        return context
+    
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy
+    template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/register.html'
