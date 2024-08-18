b030bfec1eada8c81cd707a0831719ce885efd9e
root <root@EsToDa.localdomain>
2024-08-18 16:06:46 +0300

    done

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views import View
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -3,14 +3,24 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete= models.CASCADE, related_name='books')
 
+    def __str__(self):
+        return self.name
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
+    def __str__(self):
+        return self.name
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     Library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="libraries")
+
+    def __str__(self):
+        return self.name
