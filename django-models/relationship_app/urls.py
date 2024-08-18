dd51d3234a64566ab77f49be8b93f30e3974e1a8
root <root@EsToDa.localdomain>
2024-08-18 16:49:15 +0300

    done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('library/', LibraryDetailView.as_view(), name = "library-detail"),
-    path('login/', LoginView.as_view(template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/login.html'), name = 'login'),
-    path('logout/', LogoutView.as_view(template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/logout.html'), name = 'logout'),
+    path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name = 'logout'),
     path('register/', views.register.as_view(), name = "register")
 ]
