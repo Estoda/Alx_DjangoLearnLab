760a0019469aa7f0420035ecc2c804bb9009a51a
root <root@EsToDa.localdomain>
2024-08-18 16:39:55 +0300

    done

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -12,11 +12,11 @@ def book_list_view(request):
     context = {
         'books': books,
     }
-    return render(request, '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/book_list.html', context)
+    return render(request, 'relationship_app/book_list.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_object(self):
@@ -32,4 +32,4 @@ class LibraryDetailView(DetailView):
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy
-    template_name = '/root/10thWeek/django-models/LibraryProject/relationship_app/templates/relationship_app/register.html'
+    template_name = 'relationship_app/register.html'
