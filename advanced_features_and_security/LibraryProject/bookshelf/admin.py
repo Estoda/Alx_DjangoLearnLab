c01a0fdb656b3042e3d728ccb295cbf280ef84b8
root <root@EsToDa.localdomain>
2024-08-23 18:08:57 +0300

    done

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Book
-# Register your models here.
-admin.site.register(Book)
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+admin.site.register(CustomUser, CustomUserAdmin)
