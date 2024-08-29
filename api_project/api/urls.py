dcc0351a6faeef8f05b62a0e4947ac63844dd007
root <root@EsToDa.localdomain>
2024-08-29 17:14:56 +0300

    done

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'BookList', BookViewSet)
 
 urlpatterns = [
