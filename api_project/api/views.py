bd792d56157b334766beb13d4c2548fc5a096971
root <root@EsToDa.localdomain>
2024-08-29 16:46:50 +0300

    done

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
-from .serializer import BookSerializer
+from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
