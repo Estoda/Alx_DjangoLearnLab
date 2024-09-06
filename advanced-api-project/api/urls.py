1ed8cb4acba37e373ba5bc8f3601d5e0a2e8a43c
root <root@EsToDa.localdomain>
2024-09-06 04:21:08 +0300

    done

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
