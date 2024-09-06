d604aa5675cd227d24a58bd71acec83e327f332f
root <root@EsToDa.localdomain>
2024-09-06 16:08:50 +0300

    done

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters import rest_framework
+from rest_framework import filters
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author_name', 'publication_year']
     search_fields = ['title', 'author_name']
     ordering_fields = ['title', 'publication_year']
