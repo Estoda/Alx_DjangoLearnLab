8c70b84fdececce7832aae9ad907d028d7cb977c
root <root@EsToDa.localdomain>
2024-09-09 09:48:28 +0300

    done

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    publication_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
