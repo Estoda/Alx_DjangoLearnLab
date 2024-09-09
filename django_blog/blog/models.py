a9bd3b7a0e09cd23be79ce379a9b24612a3bb307
root <root@EsToDa.localdomain>
2024-09-09 09:45:56 +0300

    done

---
+++ django_blog/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
