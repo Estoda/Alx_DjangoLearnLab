836da7e06fd064c876f2e4689143c64d1a087c47
root <root@EsToDa.localdomain>
2024-09-11 18:42:31 +0300

    done

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('post/', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
