48d8a05ceae967b192f5023cb1ab99c966d9b04d
root <root@EsToDa.localdomain>
2024-09-12 14:59:43 +0300

    doep'

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
+from taggit.managers import TaggableManager
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
@@ -12,7 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag', related_name='posts', blank=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
