1bc8e7c9857a017baeca025ca3b6315e20b3b497
root <root@EsToDa.localdomain>
2024-09-18 14:31:24 +0300

    doen

---
+++ social_media_api/social_media_api/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for social_media_api project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
+
+application = get_asgi_application()
