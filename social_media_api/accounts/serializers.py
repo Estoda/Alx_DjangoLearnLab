1d265cae582791f860fc9f0939e2192f437ee687
root <root@EsToDa.localdomain>
2024-09-18 15:20:46 +0300

    done

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 from .models import CustomUser
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['username', 'password', 'email', 'bio', 'profile_picture']
+        model = get_user_model()
+        fields = ['username', 'email', 'bio', 'profile_picture']
         extra_kwargs = {'password': {'write_only': True}}
 
         def create(self, validated_data):
-            return CustomUser.objects.create_user(**validated_data)
+            return get_user_model().objects.create_user(**validated_data)
         
 class LoginSerializers(serializers.ModelSerializer):
     username = serializers.CharField()
